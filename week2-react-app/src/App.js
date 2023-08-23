bfa7b4f9ccc13c9407f8a3315f10c25a2ff370a1
timo <hieunm@mageplaza.com>
2023-08-23 17:38:24 +0700

    new react app

--- week2-react-app/src/App.js
+++ week2-react-app/src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button, Card, Layout, Page, ResourceList} from "@shopify/polaris";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Page title={'todo'}>
+      <Layout>
+        <Layout.Section>
+            <Button>Add product</Button>
+        </Layout.Section>
+      </Layout>
+    </Page>
   );
 }
 
